7225e245eb268f7654e235fa4794504b6f01dc81
leegoang <E1398598@u.nus.edu>
2026-03-18 22:30:37 +0800

    Format code to suit check style test

--- src/main/java/seedu/modtrack/model/Command.java
+++ src/main/java/seedu/modtrack/model/Command.java
@@ -5,11 +5,12 @@ import java.util.ArrayList;
 public abstract class Command {
     protected boolean isExit = false;
 
-    public Command() {}
+    public Command() {
+    }
 
     public abstract void execute(ArrayList<Mod> list);
 
     public boolean isExit() {
-        return isExit;
+        return this.isExit;
     }
 }
